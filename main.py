7f7bf0571c6ea2e6bfc192cf43e361e5093cdfe1
A khan <awal.khan@live.co.uk>
2024-07-05 01:46:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+fruits=["apple","banana","cherry"]
+for fruit in fruits:
+  print(fruit)
+  print(fruit + " pie")
+#range of numbers
+print("The followings are the numbers from 1 to 10")
+#for number in range(1,11,2):
+ # print(number)
+  #print("Now adding each no to the total")
+
+total = 0
+for number in range(1,101):
+    total += number
+print(total)
