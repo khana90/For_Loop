217db91ecd12cdecc05352b31c9d2ca047324043
A khan <awal.khan@live.co.uk>
2024-07-05 02:09:28 +0000

    updated forloop

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-fruits=["apple","banana","cherry"]
-for fruit in fruits:
-  print(fruit)
-  print(fruit + " pie")
+#fruits=["apple","banana","cherry"]
+#for fruit in fruits:
+ # print(fruit)
+  #print(fruit + " pie")
 #range of numbers
-print("The followings are the numbers from 1 to 10")
+#print("The followings are the numbers from 1 to 10")
 #for number in range(1,11,2):
  # print(number)
   #print("Now adding each no to the total")
 
-total = 0
-for number in range(1,101):
-    total += number
-print(total)
+#total = 0
+#for number in range(1,101):
+ #   total += number
+#print(total)
+target = int(input())
+alternative_sum = 0
+for number in range(1, target + 1):
+  if number % 2 == 0:
+        # number is even
+      alternative_sum += number
+print(alternative_sum)
+
+target = int(input())
+even_sum =0
+for number in range(2, target + 1, 2):
+    even_sum += number
+print(even_sum)
